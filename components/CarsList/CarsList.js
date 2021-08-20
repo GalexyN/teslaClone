51c3dd994c2dd545abfc022c07934f9bc212bde4
GalexyN <alexneeeeewin@icloud.com>
2021-08-19 18:01:44 -0700

    Added snap functionality to allow for smoother scrolling

--- components/CarsList/CarsList.js
+++ components/CarsList/CarsList.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from './styles';
-import { View, Text, FlatList } from 'react-native';
+import { View, FlatList, Dimensions } from 'react-native';
 import cars from './cars';
 import CarItem from '../CarItem';
 
 const CarsList = () => {
-
   return (
     <View style={styles.container}>
       <FlatList
         data={cars}
-        renderItem={({ item }) => <CarItem car={item} />}
+        renderItem={({ item }) => (
+          <CarItem
+            car={item}
+          />
+          
+        )}
+        showsVerticalScrollIndicator={false}
+        snapToAlignment={'start'}
+        decelerationRate={'fast'}
+        snapToInterval={Dimensions.get('window').height}
       />
     </View>
   );
