d678b03a036ddaa8d34b71558ee54afb06d4efd9
GalexyN <alexneeeeewin@icloud.com>
2021-08-19 17:23:14 -0700

    added props to CarItem from main App component to offer reuseability for CarItem component

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+import CarItem from './components/CarItem/index';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CarItem name={"Model X"} subtitle={"Starts at 69,420"} image={require("./assets/images/ModelX.jpeg")}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/CarItem/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, ImageBackground } from 'react-native';
+import StyledButton from '../StyledButton/index';
+import styles from './styles';
+
+const CarItem = ({ name, subtitle, image }) => {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={image}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type={'primary'}
+          content={'Custom Order'}
+          onPress={() => console.warn('Pressed Custom Order')}
+        />
+        <StyledButton
+          type={'secondary'}
+          content={'Existing Inventory'}
+          onPress={() => console.warn('Pressed Existing Inventory')}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default CarItem;
